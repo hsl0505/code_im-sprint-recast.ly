54f10a8554bc461214ae7bbbf34543a5fe8398ad
Sooji Kim <yh0gogo@gmail.com>
2019-12-10 18:08:33 +0900

    VidoeListEntry code added App & VidoeList & VideoPlayer coding ongoing

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,22 +1,17 @@
-import React from 'react';
-import VideoListEntry from './VideoListEntry';
+import React from "react";
+import VideoListEntry from "./VideoListEntry";
 
-// 실제 API를 쓰게 되면 이 fakeData는 더이상 import 하지 않아야 합니다.
-import { fakeData } from './__test__/fakeData';
-console.log(fakeData)
-
-const VideoList = () => (
-  <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-  </div>
-);
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const arr = this.props.arr;
+    const mapArr = arr.map(list => (
+      <VideoListEntry key={list.id.videoId} dataList={list} />
+    ));
+    return <div className="video-list media">{mapArr}</div>;
+  }
+}
 
 export default VideoList;
--- src/components/VideoListEntry.js
+++ src/components/VideoListEntry.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const VideoListEntry = () => (
-  <div className="video-list-entry">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+const VideoListEntry = props => {
+  let list = props.dataList.snippet;
+  return (
+    <div className="video-list-entry">
+      <div className="media-left media-middle">
+        <img
+          className="media-object"
+          src={list.thumbnails.default.url}
+          alt=""
+        />
+      </div>
+      <div className="media-body">
+        <div className="video-list-entry-title">{list.title}</div>
+        <div className="video-list-entry-detail">{list.description}</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default VideoListEntry;
